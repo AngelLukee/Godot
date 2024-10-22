56266a0db693b32d062c75785cc751abcc23f649
AngelLukee <lucascosta4454@gmail.com>
2024-10-22 17:50:59 -0300

    changes

--- testes-para-godot/script/esquilo.gd
+++ testes-para-godot/script/esquilo.gd
@@ -11,12 +11,14 @@ var bulletpath = preload("res://scenes/nuts.tscn")
 @onready var shoot : Timer = $shooting
 @onready var mira_player: RayCast2D = $mira_player
 @onready var opa = $"."
-var player
+@onready var player = get_tree().get_first_node_in_group("boneco")
 
 func _ready() -> void:
 	var mirando : Vector2 = $mira_player.position
-	player = get_parent().find_child("boneco")
-	$mira_player.target_position = to_local(player.position)
+	direction = global_position.direction_to(player.global_position)
+	$mira_player.target_position = direction
+	
+	
 	
 func _physics_process(_delta):
 	match state:
@@ -34,29 +36,19 @@ func _physics_process(_delta):
 			$Label.text = "fly"
 		states.waiting:
 			$mira_player.target_position.x = player.position.x
-			print(player.position.x)
 		states.shooting:
 			shooting()
 			$Label.text = "shoot"
 	
 	move_and_slide()
+	var direction_to_player = global_position.direction_to(player.global_position)
+	$mira_player.cast_to = direction_to_player 
+	$mira_player.force_raycast_update()
+	var collision_object = $mira_player.get_collider()
 	
 	if $detector.is_colliding():
 		state = states.idle
 		$detector.enabled = false
-
-	
-
-
-
-
-
-
-
-
-
-
-
 
 func idle():
 	
@@ -95,14 +87,10 @@ func fling():
 	tween.tween_property($Sprite2D, "modulate", Color.CRIMSON, 2)
 	await tween.finished
 	velocity.y = -speed
-
-	
 	
 	if esquilo.position.y <= 470:
 		shoot.start()
 		velocity.y = 0
-		
-		
 		
 func shooting():
 	
--- testes-para-godot/script/morcego.gd
+++ testes-para-godot/script/morcego.gd
@@ -5,13 +5,32 @@ var state = states.idle
 var ondas = preload("res://scenes/ondas.tscn")
 var opa = 0
 
+func _ready() -> void:
+	pass
 
+func _process(delta: float) -> void:
+	pass
+	
+	
 func _physics_process(_delta):
-	while (4 > opa):
-		var waves = ondas.instantiate()
-		get_parent().add_child(waves)
-		waves.position = $"../primeiro marcador".global_position
-		opa += 1
-		print("foi")
-		var tween = create_tween()
-		tween.tween_property(waves, "scale", Vector2(4,4), 3)
+	pass
+
+
+func _on_timer_para_onda_timeout():
+	var waves = ondas.instantiate()
+	get_parent().add_child(waves)
+	waves.position = $"../primeiro marcador".global_position
+	print(waves.position)
+	var tween_1 = create_tween()
+	tween_1.tween_property(waves, "position", Vector2(716, 628), 2.5)
+	var tween = create_tween()
+	tween.tween_property(waves, "scale", Vector2(4,4), 3)
+	
+	var waves2 = ondas.instantiate()
+	get_parent().add_child(waves2)
+	waves2.position = $"../segundo marcado".global_position
+	waves2.rotation = -30.5
+	var tween2_1 = create_tween()
+	tween2_1.tween_property(waves2, "position", Vector2(942, 628), 3)
+	var tween2 = create_tween()
+	tween2.tween_property(waves2, "scale", Vector2(4,4), 3)
